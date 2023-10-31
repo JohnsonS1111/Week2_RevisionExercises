0ffd0fac8f4689aa960d27f336bb6ebad4b44b49
Johnson Shogbaike <G00389668@atu.ie>
2023-10-31 17:02:36 +0000

    Week 2 Exercises Revision

---
+++ src/main/java/ie/atu/week2exercises/Week2ExercisesApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week2exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2ExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2ExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week2exercises/Week2ExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2ExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
